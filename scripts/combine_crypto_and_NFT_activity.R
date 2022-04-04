b97ab866b2ea9878fa1cc2718214c8db777d904d
xerografica <xerografica@protonmail.com>
2022-04-04 08:06:00 -0700

    Remove loop from combine script and increase commenting

--- scripts/combine_crypto_and_NFT_activity.R
+++ scripts/combine_crypto_and_NFT_activity.R
@@ -3,13 +3,14 @@ head(withdrawal.df)
 head(activity.df)
 head(convert.df)
 
+# Set values in columns missing from activity.df
 activity.df$type <- "NFT"
 activity.df$coin <- "XTZ"
-head(activity.df)
 colnames(activity.df)[colnames(activity.df)=="Date"] <- "date"
+head(activity.df)
 
+# Set values in columns missing from activity.df
 withdrawal.df$type <- "CRYPTO"
-
 withdrawal.df$Token <- NA
 withdrawal.df$Buyer <- username
 withdrawal.df$Creator <- NA
@@ -29,6 +30,7 @@ head(activity.df)
 activity.df$amount <- NA
 str(activity.df)
 
+# Simple rbind to join the crypto activity and the NFT activity
 all.df <- rbind(withdrawal.df, activity.df)
 str(all.df)
 
@@ -39,43 +41,14 @@ dim(activity.df)
 # Now all are in the same dataframe, proceed. 
 all.df <- all.df[order(all.df$date), ]
 
+# Add columns to be filled through the calculations loop
 all.df$gains.crypto <- NA
 all.df$losses.crypto <- NA
 all.df$gains.NFT <- NA
 all.df$losses.NFT <- NA
+all.df$fiat.val.of.NFT.indiv <- NA
+all.df$fiat.val.of.NFT.all <- NA
 all.df$current.crypto.val <- NA
 all.df$current.crypto.vol <- NA
 
-
-
-# Broadly, per line
-head(all.df)
-
-for(r in 1:nrow(all.df)){
-  
-  # If the record is a crypto buy
-  if(all.df[r,"type"]=="CRYPTO"){
-    
-    # Run calc_fiat_value_of_crypto
-    # Will produce a value into current.crypto.val and current.crypto.vol
-  
-  }else if(all.df[r,"type"]=="NFT" && all.df[r,"Type"]=="buy"){
-    
-    # Run convert_gains.R
-    # Will affect current.crypto.vol (decrease)
-    # Selling crypto, so compare the daily value vs. current.crypto.val and add to gains.crypto or losses.crypto
-    # Add column, fiat value paid for the NFT
-    
-  }else if(all.df[r,"type"]=="NFT" && all.df[r,"Type"]=="sale"){
-    
-    # Run convert_gains.R
-    # Will affect current.crypto.vol (increase)
-    # Buying crypto, so adjust current.crypto.val based on current vs. daily
-    # THERE WILL ALSO BE A FIAT SALE AMOUNT, that is then used to buy crypto - THIS IS A GAIN
-
-  }
-  
-  
-  
-}
-
+# GO TO calc_loop.R
