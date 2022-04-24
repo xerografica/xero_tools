60307abe6457bc179e991919e3bb6474a1ee7773
xerografica <xerografica@protonmail.com>
2022-04-24 16:37:08 -0700

    Add reporting and remove the dummy dataset if not demo

--- scripts/import_crypto_buys.R
+++ scripts/import_crypto_buys.R
@@ -4,6 +4,14 @@
 # Set filename as, for example: "kucoin_withdrawal_history_<date>.csv"
 withdrawal.FN <- list.files(path = "input", pattern = "_withdrawal_history_", full.names = TRUE)
 
+# Remove the dummy version from the list if it is not running the demo
+if(demo.version == "no"){
+  
+  withdrawal.FN <- withdrawal.FN[-grep(pattern = "dummy", x = withdrawal.FN )]
+  
+}
+
+# Import all withdrawal histories and create single storage object
 markets.list <- list(); shortname <- NULL; withdrawal.df <- NULL
 for(i in 1:length(withdrawal.FN)){
   
@@ -28,10 +36,11 @@ withdrawal.df # contains a continuous dataframe
 withdrawal.df <- withdrawal.df[order(withdrawal.df$date), ]
 
 # Keep only the rows for the coin of interest
-coin
+print(paste0("You have chosen to only consider **", coin, "** transactions"))
 withdrawal.df <- withdrawal.df[grep(pattern = coin, x = withdrawal.df$coin), ]
 
 # Keep only year of interest
+print(paste0("You have chosen to only consider **", year, "** dates"))
 withdrawal.df <- withdrawal.df[grep(pattern = year, x = withdrawal.df$date), ]
 
 # Here is the details of crypto coin acquisition
